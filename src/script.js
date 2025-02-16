a2bdafaadc571aa7ab646855472b741f470df8ab
Jose Antonio <87820065+Josean001@users.noreply.github.com>
2025-02-16 14:30:12 +0100

    Create Repository

---
+++ src/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('https://jsonplaceholder.typicode.com/photos?_limit=20')
+        .then(response => response.json())
+        .then(data => {
+            const tableBody = document.getElementById("albumTableBody");
+            const recordIdsDiv = document.getElementById("recordIds");
+            let recordIdsText = "<h2>ID de los registros:</h2><p>";
+            
+            data.forEach(photo => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${photo.albumId}</td>
+                    <td>${photo.title}</td>
+                    <td><img src="${photo.url}" alt="Imagen del álbum"></td>
+                    
+                `;
+                tableBody.appendChild(row);
+                
+                recordIdsText += `${photo.id}, `;
+            });
+            
+            recordIdsText = recordIdsText.slice(0, -2) + "</p>";
+            recordIdsDiv.innerHTML = recordIdsText;
+        })
+        .catch(error => console.error("Error al obtener los datos:", error));
+});
